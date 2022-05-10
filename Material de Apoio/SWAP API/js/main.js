e1ac6064d95da6000591fc0d398653092a4c295c
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-05-10 10:56:48 -0300

    Alteração nas funções do Javascript

--- Material de Apoio/SWAP API/js/main.js
+++ Material de Apoio/SWAP API/js/main.js
@@ -1,9 +1,26 @@
-let request = "https://swapi.dev/api/people/";
+"use strict";
 
-fetch(request).then((response)=>{
-  return response.json();
-}) .then((data)=>{
-  let p = document.getElementById("test");
-  console.log(data);
-  p.innerHTML = JSON.stringify(data);
+document.addEventListener("DOMContentLoaded",()=>{
+  const apiURL = "https://swapi.dev/api/"
+  const characters = document.createElement("select")
+  characters.classList.add('form-control')
+  document.body.appendChild(characters)
+  const wrap = document.createElement("div")
+  let count = 1
+  
+  async function setCharacters(){
+    const response = await fetch(`${apiURL}people/`)
+    if (response.status >= 200 && response.status <= 299) {
+      const data = await response.json()
+      characters.add(new Option('Select a Name', ''))
+      data.results.map(elt =>{
+        const {name} = elt
+        const option = 
+        document.createElement("option")
+        option.value = count ++
+        option.innerText = name
+        characters.appendChild(option)
+      })
+    }
+  }
 })
