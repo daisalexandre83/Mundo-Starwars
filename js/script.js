039b78769e14b9bbc748ef6c908b83a3e1cc99eb
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-05-16 21:39:52 -0300

    Inserção de fumções no Javascript

--- js/script.js
+++ js/script.js
@@ -30,7 +30,31 @@ document.addEventListener("DOMContentLoaded",() =>{
     });
 });
 
-function showMe(){
+/* function showMe(){
     document.querySelector(".table").display="none";
-    
-}
+    document.querySelector(".image-star-wars").display="block";
+} */
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const apiURL="https://swapi.dev/api/"
+    const characters = document.createElement("select")
+    characters.classList.add('form-control')
+    document.body.appendChild(characters)
+    const wrap = document.createElement("div")
+    let count = 1
+
+    async function setCharacters() {
+        const response = await fetch(`${apiURL}people/`)
+        if (response.status >= 200 && response.status <= 299) {
+            const data = await response.json()
+            characters.add(new Option())
+            data.results.map(elt =>{
+                const {name} = elt
+                const option = document.createElement('option')
+                option.value = count ++
+                option.innerText = name
+                characters.appendChild(option)
+            })
+        }
+    }
+})
