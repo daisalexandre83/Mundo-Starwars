7eb1f1da9b6d0ed4204efb2ef63d0c909edeb126
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-07-04 22:48:04 -0300

    Alterações na função do Javascript

--- js/script.js
+++ js/script.js
@@ -29,17 +29,14 @@ document.addEventListener("DOMContentLoaded",() =>{
     });
 });
 
-/* document.querySelector('.action1-1').addEventListener("click",showMe); */
 
 const cells = document.querySelectorAll('td');
 for(let i = 0; i < cells.length;i++){
     cells[i].addEventListener('click',showMe)
     
-
-   
 }
 
- function showMe(){
+ /*function showMe(){
     document.querySelector(".table").style.display="none";
     document.querySelector(".image-star-wars").style.display="block";
     
@@ -56,84 +53,27 @@ for(let i = 0; i < cells.length;i++){
             })
         }
     }
+}*/
+
+function showMe() {
+    document.querySelector(".table").style.display="none";
+    document.querySelector(".image-star-wars").style.display="block";
+
+    let url = `https://swapi.dev/api/`;
+
+    fetch(url).then(function (response) {
+        response.json().then(showInformations)
+    });
 }
 
-document.addEventListener("DOMContentLoaded",()=>{
-    const apiURL="https://swapi.dev/api/"
-    const characters = document.createElement("select")
-    characters.classList.add('form-control')
-    document.body.appendChild(characters)
-    const wrap = document.createElement("div")
-    let count = 1
-    
-    async function setCharacters() {
-        const response = await fetch(`${apiURL}people/`)
-        if (response.status >= 200 && response.status <= 299) {
-            const data = await response.json()
-            // characters.add(new Option("click",tr))
-            characters.add(new Option(showMe("click",tr)))
-            data.results.map(elt =>{
-                const {name} = elt
-                const option = document.createElement('option')
-                option.value = count ++
-                option.innerText = name
-                characters.appendChild(option)
-            })
-        } else{
-            characters.add(new Option('Error, no characters found',''))
-            console.log(response.status,response.statusText)
-        }
-    }
-    async function getPlanet(id){
-        const response = await
-        fetch(`${apiURL}planets/${id}`)
-        const data = await response.json()
-        return data
-    }
-
-    async function setDetails(id) {
-        const response = await
-        fetch(`${apiURL}people/${id}/`)
-        const data = await response.json()
-
-        const{name,height, mass, hair_color,skin_color, eye_color,
-            birth_year,gender,homeworld
-        } = data
-        const planet = await
-        getPlanet(homeworld.slice(-2))
-
-        wrap.innerHTML = `
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">
-        <b>Fullname</b>:${name}</li>
-        <li class="list-group-item">
-        <b>Height</b>:${height}</li>
-        <li class="list-group-item">
-        <b>Mass</b>:${mass}</li>
-        <li class="list-group-item">
-        <b>Hair Color</b>:${hair_color}</li>
-        <li class="list-group-item">
-        <b>Skin Color</b>:${skin_color}</li>
-        <li class="list-group-item">
-        <b>Eye Color</b>:${eye_color}</li>
-        <li class="list-group-item">
-        <b>Birth Year</b>:${birth_year}</li>
-        <li class="list-group-item">
-        <b>Gender</b>:${gender}</li>
-        <li class="list-group-item">
-        <b>Homeworld</b>:${planet.name}- Population ${planet.residents.lenght}</li>
-        </ul>
-        `;
-        characters.after(wrap)
-    }
-    characters.addEventListener("change",e =>
-    {
-        const id = e.currentTarget.options[e.currentTarget.selectedIndex].value
-        wrap.innerText = ''
-        setDetails(id)
-    })
-    setCharacters()
-})
-
+function showInformations(dados) {
+    cells.innerHTML = `<p>Name:${dados.name}</p>
+                        <p>Heigth:${dados.height}</p>
+                        <p>Mass:${dados.mass}</p>
+                        <p>Hair Color:${dados.hair_color}</p>
+                        <p>Skin Color:${dados.skin_color}</p>
+                        <p>Eye Color:${dados.eye_color}</p>
+                        <p>Homeworld:${dados.homeworld}</p>`
+}
 
 
