45bac7758b9cbbbde4fff5d252bbb9f401a1555e
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-08-22 10:10:54 -0300

    Alterações nas funções do Javascript

--- js/script.js
+++ js/script.js
@@ -41,6 +41,7 @@ function showMe() {
     document.querySelector(".table").style.display="none";
     document.querySelector(".content").style.display="block";    // document.querySelector(".image-star-wars").style.display="block";
     const name = this.parentNode.getAttribute("data-name");
+    let url = `https://swapi.dev/api/people?search=${name}`;
    
     fetch(url)
     .then(function(response) {
@@ -71,6 +72,7 @@ function showInformations(dados) {
     const info = document.querySelector('.info');
 
     info.innerHTML = `<p class="date-name">${dados.name}</p>
+                        <div class="line"></div>
                         <h3 class="character">CHARACTERISTICS</h3>
                         <p class="itens-character">Name:<span class="itens-description">${dados.name}</span></p>
                         <p class="itens-character">Heigth:<span class="itens-description">${dados.height}</span></p>
@@ -78,7 +80,13 @@ function showInformations(dados) {
                         <p class="itens-character">Hair Color:<span class="itens-description">${dados.hair_color}</span></p>
                         <p class="itens-character">Skin Color:<span class="itens-description">${dados.skin_color}</span></p>
                         <p class="itens-character">Eye Color:<span class="itens-description">${dados.eye_color}</span></p>
-                        <p class="itens-character">Homeworld:<span class="itens-description">${dados.planet.name}</span></p>`
+                        <div>
+                            <p><p>
+                            <h3 class="character2">FILMS<h3>
+                            <p class="itens-character">${dados.films}<p>
+                        </div>`
+                        // <p class="itens-character">Homeworld:<span class="itens-description">${dados.planet.name}</span></p>`
+
 }
 
 
