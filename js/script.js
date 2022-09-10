5a4a3f4f647bbfba1679d9b309d027c7a6016fea
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-09-09 22:00:44 -0300

    Alterações na função do Javascript

--- js/script.js
+++ js/script.js
@@ -37,11 +37,14 @@ for(let i = 0; i < cells.length;i++){
 }
 
 
-function showMe() {
+async function showMe() {
     document.querySelector(".table").style.display="none";
     document.querySelector(".content").style.display="block";    // document.querySelector(".image-star-wars").style.display="block";
     const name = this.parentNode.getAttribute("data-name");
-    let url = `https://swapi.dev/api/people?search=${name}`;
+    // let url = `https://swapi.dev/api/people?search=${name}`;
+    let url = await fetch(`https://swapi.dev/api/people?search=${name}`) ;
+    let json = await url.json();
+    const planet = await getPlanet(homeworld.slice(-2))
    
     fetch(url)
     .then(function(response) {
@@ -72,7 +75,7 @@ function showInformations(dados) {
     const info = document.querySelector('.info');
 
     info.innerHTML = `<p class="date-name">${dados.name}</p>
-                        <div class="line"></div>
+                        <div class="line l-first"></div>
                         <h3 class="character">CHARACTERISTICS</h3>
                         <p class="itens-character">Name:<span class="itens-description">${dados.name}</span></p>
                         <p class="itens-character">Heigth:<span class="itens-description">${dados.height}</span></p>
@@ -80,12 +83,8 @@ function showInformations(dados) {
                         <p class="itens-character">Hair Color:<span class="itens-description">${dados.hair_color}</span></p>
                         <p class="itens-character">Skin Color:<span class="itens-description">${dados.skin_color}</span></p>
                         <p class="itens-character">Eye Color:<span class="itens-description">${dados.eye_color}</span></p>
-                        <div>
-                            <p><p>
-                            <h3 class="character2">FILMS<h3>
-                            <p class="itens-character">${dados.films}<p>
-                        </div>`
-                        // <p class="itens-character">Homeworld:<span class="itens-description">${dados.planet.name}</span></p>`
+                        <p class="itens-character">Homeworld:<span class="itens-description">${dados.homeworld}</span></p>
+                         `
 
 }
 
