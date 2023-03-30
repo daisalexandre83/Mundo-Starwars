5703764eff4c9e75741f18f0ba81e38dfd581346
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2023-03-30 19:01:06 -0300

    Alterações na função do Javacript

--- js/script.js
+++ js/script.js
@@ -87,23 +87,31 @@ async function showInformations(dados) {
 
 info.innerHTML = `<p class="date-name">${dados.name}</p>
                     let buttons = document.querySelectorAll(".selectionSection button"); 
-                   <div class="container">
-                   <h3 class="character-title">CHARACTERISTICS</h3>
-                   <div class="line">
-                   </div>
-                   <p class="itens-character">Name:<span class="itens-description">${dados.name}</span></p>
-                   <p class="itens-character">Heigth:<span class="itens-description">${dados.height}</span></p>
-                   <p class="itens-character">Mass:<span class="itens-description">${dados.mass}</span></p>
-                   <p class="itens-character">Hair Color:<span class="itens-description">${dados.hair_color}</span></p>
-                   <p class="itens-character">Skin Color:<span class="itens-description">${dados.skin_color}</span></p>
-                   <p class="itens-character">Eye Color:<span class="itens-description">${dados.eye_color}</span></p>
-                   <p class="itens-character">Planet:<span class="itens-description">${planet.name}</span></p>
-                   <!--h1>FILMS</h1-->
-                   <p class="itens-character">Film:<span class="itens-description">${getFilms4.title}</span></p>
-                   <p class="itens-character">Film:<span class="itens-description">${getFilm5.title}</span></p>
-                   <p class="itens-character">Film:<span class="itens-description">${getFilm6.title}</span></p>
-                   
-                   </div>
+                    <div class="container">
+                     <div class="selection-section">
+                      <button type="button" data-number="1" class="active">CHARACTERISTICS<span><div class="line"></div><span></button>
+                      <button type="button" data-number="2" class="active">FILMS</button>
+                      <button type="button" data-number="3" class="active">STARSHIPS</button>
+                     <div>
+                     <div class="content-section">
+                      <div class="content" data-number="1">
+                       <p class="itens-character">Name:<span class="itens-description">${dados.name}</span></p>
+                       <p class="itens-character">Heigth:<span class="itens-description">${dados.height}</span></p>
+                       <p class="itens-character">Mass:<span class="itens-description">${dados.mass}</span></p>
+                       <p class="itens-character">Hair Color:<span class="itens-description">${dados.hair_color}</span></p>
+                       <p class="itens-character">Skin Color:<span class="itens-description">${dados.skin_color}</span></p>
+                       <p class="itens-character">Eye Color:<span class="itens-description">${dados.eye_color}</span></p>
+                       <p class="itens-character">Planet:<span class="itens-description">${planet.name}</span></p>
+                      </div>
+                     </div>
+                     <div class="content" data-number ="2">
+                      <p class="itens-character">Film:<span class="itens-description">${getFilms4.title}</span></p>
+                      <p class="itens-character">Film:<span class="itens-description">${getFilm5.title}</span></p>
+                      <p class="itens-character">Film:<span class="itens-description">${getFilm6.title}</span></p>
+                      </div>
+                      <!--h3 class="container-titles">CHARACTERISTICS</h3>
+                      <h1 class="container-titles>FILMS</h1-->
+                    </div>
                     `
    
      const info2 = document.querySelector('.info');                    
