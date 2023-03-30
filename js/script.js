9a59d7e17ffd2308383fcb8ac788972b39b11724
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2023-03-29 23:40:28 -0300

    Alterações na função do Javascript

--- js/script.js
+++ js/script.js
@@ -87,21 +87,22 @@ async function showInformations(dados) {
 
 info.innerHTML = `<p class="date-name">${dados.name}</p>
                     let buttons = document.querySelectorAll(".selectionSection button"); 
-                   <div class="">
-                   </div>
+                   <div class="container">
+                   <h3 class="character-title">CHARACTERISTICS</h3>
                    <div class="line">
-                    <h3 class="character-title">CHARACTERISTICS</h3>
-                    <p class="itens-character">Name:<span class="itens-description">${dados.name}</span></p>
-                    <p class="itens-character">Heigth:<span class="itens-description">${dados.height}</span></p>
-                    <p class="itens-character">Mass:<span class="itens-description">${dados.mass}</span></p>
-                    <p class="itens-character">Hair Color:<span class="itens-description">${dados.hair_color}</span></p>
-                    <p class="itens-character">Skin Color:<span class="itens-description">${dados.skin_color}</span></p>
-                    <p class="itens-character">Eye Color:<span class="itens-description">${dados.eye_color}</span></p>
-                    <p class="itens-character">Planet:<span class="itens-description">${planet.name}</span></p>
-                    <h1>FILMS</h1>
-                    <p class="itens-character">Film:<span class="itens-description">${getFilms4.title}</span></p>
-                    <p class="itens-character">Film:<span class="itens-description">${getFilm5.title}</span></p>
-                    <p class="itens-character">Film:<span class="itens-description">${getFilm6.title}</span></p>
+                   </div>
+                   <p class="itens-character">Name:<span class="itens-description">${dados.name}</span></p>
+                   <p class="itens-character">Heigth:<span class="itens-description">${dados.height}</span></p>
+                   <p class="itens-character">Mass:<span class="itens-description">${dados.mass}</span></p>
+                   <p class="itens-character">Hair Color:<span class="itens-description">${dados.hair_color}</span></p>
+                   <p class="itens-character">Skin Color:<span class="itens-description">${dados.skin_color}</span></p>
+                   <p class="itens-character">Eye Color:<span class="itens-description">${dados.eye_color}</span></p>
+                   <p class="itens-character">Planet:<span class="itens-description">${planet.name}</span></p>
+                   <!--h1>FILMS</h1-->
+                   <p class="itens-character">Film:<span class="itens-description">${getFilms4.title}</span></p>
+                   <p class="itens-character">Film:<span class="itens-description">${getFilm5.title}</span></p>
+                   <p class="itens-character">Film:<span class="itens-description">${getFilm6.title}</span></p>
+                   
                    </div>
                     `
    
