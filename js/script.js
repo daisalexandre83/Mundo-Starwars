9b569cf1bab0ee5dc9d46ad7dc2ace0c66ffe0ae
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2023-06-23 22:07:13 -0300

    Alterações na função

--- js/script.js
+++ js/script.js
@@ -56,66 +56,70 @@ async function getPlanet(url){
     const data = await response.json()
     return data;
 }
-
-async function getNameFilm4(){
-    const results1 = await fetch("https://swapi.dev/api/films/4/")
-    const data = await results1.json()
-    return data;
+//Função para mostrar o ícone das características do presonagem
+function showWordCharacter(clicked_element) {
+    // let charactersAnakin = document.getElementsByClassName(el).style.display;
+    let characters = clicked_element.getAttribute("data-name");
+    // if (display == "none") {
+    //    document.getElementById(el).style.display = 'block'; 
+    // }
 }
 
-async function getNameFilm5(){
-    const results2 = await fetch("https://swapi.dev/api/films/5/")
-    const data = await results2.json()
-    return data;
-}
+// async function getNameFilm4(){
+//     const results1 = await fetch("https://swapi.dev/api/films/4/")
+//     const data = await results1.json()
+//     return data;
+// }
 
-async function getNameFilm6(){
-    const results3 = await fetch("https://swapi.dev/api/films/6/")
-    const data = await results3.json()
-    return data;
-}
+// async function getNameFilm5(){
+//     const results2 = await fetch("https://swapi.dev/api/films/5/")
+//     const data = await results2.json()
+//     return data;
+// }
+
+// async function getNameFilm6(){
+//     const results3 = await fetch("https://swapi.dev/api/films/6/")
+//     const data = await results3.json()
+//     return data;
+// }
 
 
 async function showInformations(dados) {
     const info = document.querySelector('.info');
     const planet = await getPlanet(dados.homeworld);
-    const getFilms4 = await getNameFilm4(dados);
-    const getFilm5 = await getNameFilm5(dados);
-    const getFilm6 = await getNameFilm6(dados);
+    // const getFilms4 = await getNameFilm4(dados);
+    // const getFilm5 = await getNameFilm5(dados);
+    // const getFilm6 = await getNameFilm6(dados);
 
-let buttons = document.querySelectorAll(".select-section button");
+// let buttons = document.querySelectorAll(".select-section button");
 
-for (let button of buttons) {
-    button.addEventListener('click',e =>{
-        const et = e.target;
-        const active = document.querySelector(".active");
-        if (active) {
-            active.classList.remove("active");
-        }
-        et.classList.add("active");
-        let allContent = document.querySelectorAll('.content ');
+// for (let button of buttons) {
+//     button.addEventListener('click',e =>{
+//         const et = e.target;
+//         const active = document.querySelector(".active");
+//         if (active) {
+//             active.classList.remove("active");
+//         }
+//         et.classList.add("active");
+//         let allContent = document.querySelectorAll('.content ');
 
-        for (let content of allContent) {
-            if (content.getAttribute('data-number')=== button.getAttribute('data-number')) {
-                content.style.display = 'block';
-            } else {
-                content.style.display = 'none';
-            }
+//         for (let content of allContent) {
+//             if (content.getAttribute('data-number')=== button.getAttribute('data-number')) {
+//                 content.style.display = 'block';
+//             } else {
+//                 content.style.display = 'none';
+//             }
             
-        }
-    })
+//         }
+//     })
     
-}
+// }
+
+
 
 info.innerHTML = `<p class="date-name">${dados.name}</p>
                    <div class="container">
-                    <div class="select-section">
-                    <button type="button" data-number="2" class="active">FILMS</button>
-                    <button type="button" data-number="3" class="active">STARSHIPS</button>
-                    </div>
                     <div class="content-section">
-                    <div class="content" data-number="1">
-                    <button type="button" data-number="1" class="active">CHARACTERISTICS</button>
                       <p class="itens-character">Name:<span class="itens-description">${dados.name}</span></p>
                       <p class="itens-character">Heigth:<span class="itens-description">${dados.height}</span></p>
                       <p class="itens-character">Mass:<span class="itens-description">${dados.mass}</span></p>
@@ -124,12 +128,7 @@ info.innerHTML = `<p class="date-name">${dados.name}</p>
                       <p class="itens-character">Eye Color:<span class="itens-description">${dados.eye_color}</span></p>
                       <p class="itens-character">Planet:<span class="itens-description">${planet.name}</span></p>
                      </div>
-                     <div class="content" data-number="2">
-                      <p class="itens-character">Film:<span class="itens-description">${getFilms4.title}</span></p>
-                      <p class="itens-character">Film:<span class="itens-description">${getFilm5.title}</span></p>
-                      <p class="itens-character">Film:<span class="itens-description">${getFilm6.title}</span></p>
-                     </div>
-                    </div>
+                    
                    </div> 
                    
                     `
