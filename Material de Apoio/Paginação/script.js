3f15e2fa6a81dd9dc8c0cc73c8f4643ec34da65f
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-01-29 21:21:31 -0300

    Inserção do Javascript do Material de Apoio.

---
+++ Material de Apoio/Paginação/script.js
@@ -0,0 +1,43 @@
+const paginationNumbers = document.getElementById("pagination-numbers");
+const paginatedList = document.getElementById("paginated-list");
+const listItems =paginatedList.querySelectorAll("li");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+const paginationLimit = 10;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+const disableButton = (button) =>{
+    button.classList.add("disabled");
+    button.setAttribute("disabled",true);
+};
+
+const enableButton = (button) =>{
+    button.classList.remove("disabled");
+    button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () =>{
+    if(currentPage === 1){
+        disableButton(prevButton);
+    }else{
+        enableButton(prevButton);
+    }
+
+    if(pageCount === currentPage){
+        disableButton(nextButton);
+    }else{
+        enableButton(nextButton);
+    }
+};
+
+const handleActivePageNumber = () =>{
+    document.querySelectorAll(".pagination-number").forEach((button)=>{
+        button.classList.remove("active");
+        const pageIndex = Number(button.getAttribute("page-index"));
+        if (pageIndex == currentPage) {
+            button.classList.add("active");
+        }
+    });
+};
