801ca565527ac66b6de5d339eff76ee563ff3a74
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-01-30 22:23:13 -0300

    Inserção de funções no Javascript

--- Material de Apoio/Paginação/script.js
+++ Material de Apoio/Paginação/script.js
@@ -41,3 +41,58 @@ const handleActivePageNumber = () =>{
         }
     });
 };
+
+const appendPageNumber = (index) =>{
+    const pageNumber = document.createElement("button");
+    pageNumber.className = "pagination-number";
+    pageNumber.innerHTML = index;
+    pageNumber.setAttribute("page-index",index);
+    pageNumber.setAttribute("arial-label","Page" + index);
+    paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () =>{
+    for (let i = 1; i <= pageCount; i++) {
+        appendPageNumber(i);
+    }
+};
+
+const setCurrentPage = (pageNum) =>{
+    currentPage = pageNum;
+
+    handleActivePageNumber();
+    handlePageButtonsStatus();
+
+    const prevRange = (pageNum - 1) * paginationLimit;
+    const currRange = pageNum * paginationLimit;
+
+    listItems.forEach((item,index) =>{
+        item.classList.add("hidden");
+        if (index >= prevRange && index < currRange) {
+            item.classList.remove("hidden");
+        }
+    });
+};
+
+window.addEventListener("load",() =>{
+    getPaginationNumbers();
+    setCurrentPage(1);
+
+    prevButton.addEventListener("click", () =>{
+        setCurrentPage(currentPage -1);
+    });
+
+    nextButton.addEventListener("click", () =>{
+        setCurrentPage(currentPage + 1);
+    });
+
+    document.querySelectorAll(".pagination-number").forEach((button) =>{
+        const pageIndex = Number(button.getAttribute("page-index"));
+
+        if (pageIndex) {
+            button.addEventListener("click",() =>{
+                setCurrentPage(pageIndex);
+            });
+        }
+    })
+})
