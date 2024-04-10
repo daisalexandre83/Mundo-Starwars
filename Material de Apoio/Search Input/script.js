6a83ce8fb751dc705f1ca7e855acf40c6cfd1514
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-04-09 22:20:25 -0300

    Inserção de função em JS

--- Material de Apoio/Search Input/script.js
+++ Material de Apoio/Search Input/script.js
@@ -8,9 +8,26 @@ function setLanguage(prop) {
     document
     .querySelector(".control-panel")
     .setAttribute("style","direction:rtl");
+
+    if(prop == "en"){
+        document.querySelector("#title").textContext ="Control panel";
+        document.querySelector("#title").textContent="Control panel";
+        document
+        .querySelector(".search-container input")
+        .setAttribute("placeholder",'Type something');
+        document
+        .querySelector(".control-panel")
+        .setAttribute("style", "direction:ltr");
+    
+    }
 }
 
-if(prop == "en"){
-    document.querySelector("#title").textContext ="Control panel";
-    
+function toggleDark() {
+    document.querySelector(".control-panel").classList.toggle("dark");
+    if (document.querySelector(".control-panel").classList.contains("dark")) {
+        document.querySelector("#dark-icon").innerHTML="light_mode";
+    }else{
+        document.querySelector("#dark-icon").innerHTML="dark_mode";
+    }
 }
+
