f80fa0852356cca659b9a95d6fb6c49bd7e99e5d
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-05-29 20:01:20 -0300

    Inserção de função no JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 let isMoviesVisible = false;
 
-document.addEventListener("DOMContentLoaded",() =>{
-    document.querySelectorAll("#search-input").forEach((inputField)=>{
+// document.addEventListener("DOMContentLoaded",() =>{
+//         document.querySelectorAll("#search-input").forEach((inputField)=>{
+//         const tableRows = inputField.closest("table")
+//         .querySelectorAll("tbody > tr");
+//         const headerCell = inputField.closest("th");
+//         const otherHeaderCells = 
+//         headerCell.closest("tr").children;
+//         const columnIndex = 
+//         Array.from(otherHeaderCells).indexOf(headerCell);
+//         const searchableCells = 
+//         Array.from(tableRows).map(
+//             (row) => row.querySelectorAll("td")
+//             [columnIndex]
+//          );
+//          inputField.addEventListener("input",() =>{
+//              const searchQuery = inputField.value.toLowerCase();
+
+//              for (const tableCell of searchableCells) {
+//                 const row = tableCell.closest("tr");
+//                 const value = 
+//                  tableCell.textContent.toLocaleLowerCase().replace(",","");
+//                  row.style.visibility = null;
+
+//                  if (value.search(searchQuery) === -1) {
+//                      row.style.visibility = "collapse";
+//                  }
+//              }
+//          });
+//     });
+// });
+
+function tablePersons(){
+    document.querySelectorAll("#search-input2").forEach((inputField)=>{
         const tableRows = inputField.closest("table")
         .querySelectorAll("tbody > tr");
         const headerCell = inputField.closest("th");
@@ -29,7 +60,8 @@ document.addEventListener("DOMContentLoaded",() =>{
              }
          });
     });
-});
+    
+}
 
 
 const cells = document.querySelectorAll('td');
