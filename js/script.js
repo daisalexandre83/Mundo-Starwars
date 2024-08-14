f1a9e15918ec74f0c023070fc03ffc66a2627f35
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-08-13 22:18:08 -0300

    Exclusão de função em JS

--- js/script.js
+++ js/script.js
@@ -1,35 +1,6 @@
 let isMoviesVisible = false;
 
-function tablePersons(e){
-    document.querySelectorAll("#search-input").forEach((inputField)=>{
-                const tableRows = inputField.closest("table")
-                .querySelectorAll("tbody > tr");
-                const headerCell = inputField.closest("th");
-                const otherHeaderCells = 
-                headerCell.closest("tr").children;
-                const columnIndex = 
-                Array.from(otherHeaderCells).indexOf(headerCell);
-                const searchableCells = 
-                Array.from(tableRows).map(
-                    (row) => row.querySelectorAll("td")
-                    [columnIndex]
-                 );
-                 inputField.addEventListener("input",() =>{
-                     const searchQuery = inputField.value.toLowerCase();
-        
-                     for (const tableCell of searchableCells) {
-                        const row = tableCell.closest("tr");
-                        const value = 
-                         tableCell.textContent.toLocaleLowerCase().replace(",","");
-                         row.style.visibility = null;
-        
-                         if (value.search(searchQuery) === -1) {
-                             row.style.visibility = "collapse";
-                         }
-                     }
-                 });
-            });
-}
+
 
 
 document.addEventListener("DOMContentLoaded",() =>{
