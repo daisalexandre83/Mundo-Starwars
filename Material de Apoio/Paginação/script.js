962e2a894559a87f48eb36a1fed780b1c9d4a276
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-04-10 22:50:19 -0300

    Criação de funções no arquivo script.js

--- Material de Apoio/Paginação/script.js
+++ Material de Apoio/Paginação/script.js
@@ -7,6 +7,65 @@ function populateList() {
    list.innerHTML = data.join("")
 
    // console.log(data)
+
+   return data
 }
 
-populateList()
+const data = populateList()
+
+//Exemplo
+// let perPage = 3
+// const state = {
+//    page:1,
+//    perPage,
+//    totalPage:Math.ceil(data.length /perPage)
+// }
+
+let perPage = 5
+const state = {
+   page:1,
+   perPage,
+   totalPage:Math.ceil(data.length /perPage)
+}
+
+
+
+console.log(state.totalPage)
+
+const controls = {
+   next() {
+      state.page++;
+
+      const lastPage = state.page > state.totalPage
+      if (lastPage) {
+         state.page--;
+      }
+   },
+   prev() {
+      state.page--
+
+      if (state.page < 1) {
+         state.page++
+      }
+   },
+   goTo(page) {
+      if (page < 1) {
+         page = 1
+      }
+      
+      state.page = page
+
+      if (page > state.totalPage) {
+         state.page = state.totalPage
+      }
+   }
+}
+
+console.log(state.page)
+controls.goTo(90)
+console.log(state.page)
+// controls.next()
+// console.log(state.page)
+// controls.prev()
+// console.log(state.page)
+
