95d795839c835d5517c47029bbaec8ad5538a6f0
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-04-29 23:36:14 -0300

    Criação e alteração de funções no JS do arquivo script.js

--- Material de Apoio/Paginação/script.js
+++ Material de Apoio/Paginação/script.js
@@ -24,11 +24,12 @@ const data = Array.from({length:100})
 //    totalPage:Math.ceil(data.length /perPage)
 // }
 
-let perPage = 5
+let perPage = 5;
 const state = {
    page:1,
    perPage,
-   totalPage: Math.ceil(data.length / perPage)
+   totalPage: Math.ceil(data.length / perPage),
+   maxVisibleButtons:5
 }
 
 const html = {
@@ -60,7 +61,7 @@ const controls = {
          page = 1
       }
       
-      state.page = page
+      state.page = +page
 
       if (page > state.totalPage) {
          state.page = state.totalPage
@@ -102,7 +103,7 @@ const list = {
 
       let page = state.page - 1;
       let start = page  * state.perPage;
-      let end  = start +  state.perPage
+      let end  = start + state.perPage;
 
       const paginatedItems = data.slice(start,end)
 
@@ -111,8 +112,55 @@ const list = {
 }
 
 const buttons = {
-   create() {},
-   update() {}
+   element: html.get('.pagination .numbers'),
+   create(number) {
+      const button = document.createElement('div')
+
+      button.innerHTML = number;
+
+      if (state.page == number) {
+         button.classList.add('active')
+      }
+
+      button.addEventListener('click', (event) => {
+         const page = event.target.innerText
+         
+         controls.goTo(page)
+         update()
+      })
+
+      buttons.element.appendChild(button)
+
+   },
+   update() {
+      buttons.element.innerHTML = "";
+      const {maxLeft,maxRight} = buttons.calculateMaxVisible()
+
+      // console.log(maxLeft,maxRight)
+      for (let page = maxLeft; page <= maxRight; page ++) {
+         console.log('button')
+         buttons.create(page)
+      }
+   },
+   calculateMaxVisible(){
+      const {maxVisibleButtons} = state
+      let maxLeft = (state.page - Math.floor(maxVisibleButtons/2))
+      let maxRight = (state.page + Math.floor(maxVisibleButtons/2))
+
+      if (maxLeft < 1) {
+         maxLeft = 1
+         maxRight = maxVisibleButtons
+      }
+
+      if (maxRight > state.totalPage) {
+         maxLeft = state.totalPage - (maxVisibleButtons -1)
+         maxRight = state.totalPage
+
+         if (maxLeft < 1) maxLeft =1
+      }
+
+      return (maxLeft,maxRight)
+   }
 }
 
 function update() {
@@ -122,7 +170,7 @@ function update() {
 
 function init() {
   update() 
-   controls.createListeners()
+  controls.createListeners()
 }
 
 init()
